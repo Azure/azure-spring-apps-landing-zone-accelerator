4d87e599917879c3d423a97a6aaae4969ca6a714
Troy Ault <aultt@users.noreply.github.com>
2021-01-29 17:47:34 -0500

    Adding Workstation Configuration

---
+++ installdependancies.ps1
@@ -0,0 +1,12 @@
+choco install azure-cli -y
+choco install zulu8 -y
+choco install maven -y
+choco install mysql-cli -y
+choco install vscode -y
+choco install vscode-java -y
+choco install jq -y
+choco install git -y
+choco install wsl2 -y
+
+#Requires Reboot before running
+choco install wsl-ubuntu-2004 -y
