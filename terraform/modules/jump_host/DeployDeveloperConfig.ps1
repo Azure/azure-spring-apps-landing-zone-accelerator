47f7a414a7443d665419d2fda464f1eae931b249
David Apolinar <dapolloxp@users.noreply.github.com>
2021-02-05 14:39:13 -0500

    Update DeployDeveloperConfig.ps1
    adding force parameter.

--- terraform/modules/jump_host/DeployDeveloperConfig.ps1
+++ terraform/modules/jump_host/DeployDeveloperConfig.ps1
@@ -1,5 +1,5 @@
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-Install-Module cChoco -MaximumVersion 2.4.1.0
+Install-Module cChoco -MaximumVersion 2.4.1.0 -force
 Install-Module ComputerManagementdsc
 Set-Location c:\
 ./DeveloperWorkstation_Config.ps1
