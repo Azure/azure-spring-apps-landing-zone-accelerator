cf21f8eca7a1476ec7935691608dcec3f163d317
David Apolinar <dapolloxp@users.noreply.github.com>
2021-02-05 15:02:12 -0500

    Update DeployDeveloperConfig.ps1
    Adding logging.

--- terraform/modules/jump_host/DeployDeveloperConfig.ps1
+++ terraform/modules/jump_host/DeployDeveloperConfig.ps1
@@ -1,5 +1,11 @@
+$ErrorActionPreference="SilentlyContinue"
+Stop-Transcript | out-null
+$ErrorActionPreference = "Continue"
+Start-Transcript -path C:\WindowsAzure\Logs\deploydeveloperconfig.log -append
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 Install-Module cChoco -MaximumVersion 2.4.1.0 -force
 Install-Module ComputerManagementdsc
 Set-Location c:\
 ./DeveloperWorkstation_Config.ps1
+Stop-Transcript
