985ec7b5c7b3232ae04771a3a9f9c6591228c19b
David Apolinar <dapolloxp@users.noreply.github.com>
2021-02-05 15:53:46 -0500

    Update DeployDeveloperConfig.ps1
    Adding choco script

--- terraform/modules/jump_host/DeployDeveloperConfig.ps1
+++ terraform/modules/jump_host/DeployDeveloperConfig.ps1
@@ -4,6 +4,7 @@ $ErrorActionPreference = "Continue"
 Start-Transcript -path C:\WindowsAzure\Logs\deploydeveloperconfig.log -append
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 Install-Module cChoco -MaximumVersion 2.4.1.0 -force
 Install-Module ComputerManagementdsc
 copy DeveloperWorkstation_Config.ps1 -destination C:\
