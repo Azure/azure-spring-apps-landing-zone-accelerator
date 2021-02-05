b55671a7737e07accb94601e084c9a041bddad25
David Apolinar <dapolloxp@users.noreply.github.com>
2021-02-05 13:00:45 -0500

    Update DeployDeveloperConfig.ps1
    Removing prompt parameter.

--- terraform/modules/jump_host/DeployDeveloperConfig.ps1
+++ terraform/modules/jump_host/DeployDeveloperConfig.ps1
@@ -1,4 +1,4 @@
-Install-Module cChoco -MaximumVersion 2.4.1.0 -Confirm
-Install-Module ComputerManagementdsc -Confirm
+Install-Module cChoco -MaximumVersion 2.4.1.0
+Install-Module ComputerManagementdsc
 Set-Location c:\
 ./DeveloperWorkstation_Config.ps1
