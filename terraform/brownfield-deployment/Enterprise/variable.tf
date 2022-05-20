c3aa2c49827f85d19a866bad1fcd937285782e41
Luis <6314335+luisfeliz79@users.noreply.github.com>
2022-05-20 13:55:20 -0400

    updated settings files

--- terraform/brownfield-deployment/Enterprise/variable.tf
+++ terraform/brownfield-deployment/Enterprise/variable.tf
@@ -1,3 +1,5 @@
+
+## General
 variable "subscription" {
     type = string
     description = "Azure Subscription"
@@ -12,45 +14,21 @@ variable "resource_group_name" {
     description = "Name of the Resource Group where resources will be deployed"
     default     = ""
 }
-variable "azurespringcloudvnetrg" {
-    type    = string
-    description = "Name of the Virtual Network Resource Group where resources will be deployed"
-    default = ""
-}
 variable "sc_service_name" {
     type        = string
     description = "Spring Cloud Service Name"
     default     =  ""
 }
-variable "vnet_spoke_name" {
-     type    = string
-    description = "Name of the Spoke Virtual Network name(e.g. vnet-spoke)"
-    default = ""
-}
-variable "app_subnet_id" {
-    type    = string
-    description = "Name of the SubNet to be used by Spring Cloud App Service (e.g snet-app)"
-    default = ""
-}
-variable "service_runtime_subnet_id" {
-    type    = string
-    description = "Name of the SubNet to be used by Spring Cloud runtime Service (e.g snet-runtime)"
-    default = ""
-}
+
 variable "app_insights_name" {
     type        = string
     description = "App Insights Name"
     default     =  ""
 }
-variable "sc_law_id" {
-    type    = string
-    description = "Name of the Azure Log Analytics workspace to be used for storing diagnostic logs(e.g la-cb5sqq6574o2a)"
-    default = ""
-}
 
 variable "sc_cidr" {
     type        = list
-    description = "CIDR Ranges from your Virtual network to be used by Azure Spring Cloud(e.g XX.X.X.X/16,XX.X.X.X/16,XX.X.X.X/16)"
+    description = "3 CIDR ranges (at least /16) which are used to host the Spring Cloud backend infrastructure, which must not overlap with any existing CIDR ranges in the Subnet (e.g XX.X.X.X/16,XX.X.X.X/16,XX.X.X.X/16)"
     default     = ["10.3.0.0/16", "10.4.0.0/16", "10.5.0.1/16"]
 }
 variable "tags" {
@@ -61,5 +39,38 @@ variable "tags" {
         BusinesUnit = "Finance"
     }
 }
+
+
+
+
+## Existing Infrastructure
+variable "azurespringcloudvnetrg" {
+    type    = string
+    description = "The name of the resource group where the existing VNET resides"
+    default = ""
+}
+
+variable "vnet_spoke_name" {
+     type    = string
+    description = "Name of the existing Spoke Virtual Network name(e.g. vnet-spoke)"
+    default = ""
+}
+variable "app_subnet_id" {
+    type    = string
+    description = "Name of the existing SubNet to be used by Spring Cloud App Service (e.g snet-app)"
+    default = ""
+}
+variable "service_runtime_subnet_id" {
+    type    = string
+    description = "Name of the existing SubNet to be used by Spring Cloud runtime Service (e.g snet-runtime)"
+    default = ""
+}
+
+variable "sc_law_id" {
+    type    = string
+    description = "Name of the existing Azure Log Analytics workspace to be used for storing diagnostic logs(e.g la-cb5sqq6574o2a)"
+    default = ""
+}
+
 
 
