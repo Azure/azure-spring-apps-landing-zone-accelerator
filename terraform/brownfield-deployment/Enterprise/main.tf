c72a96815795f1197b666bc021096733fee6460a
Luis <6314335+luisfeliz79@users.noreply.github.com>
2022-05-23 20:51:19 -0400

    added Tanzu components for brownfield-Enterprise

--- terraform/brownfield-deployment/Enterprise/main.tf
+++ terraform/brownfield-deployment/Enterprise/main.tf
@@ -85,21 +85,21 @@ resource "azurerm_monitor_diagnostic_setting" "sc_diag" {
 # Begin Tanzu Components
 
 
-## Create build pack
-resource "azurerm_spring_cloud_builder" "asc-builder" {
-  name                    = "buildpack"
-  spring_cloud_service_id = azurerm_spring_cloud_service.sc.id
+resource "azurerm_spring_cloud_build_pack_binding" "appinsights-binding" {
+  name                    = "appinsights-binding"
+  spring_cloud_builder_id = "${azurerm_spring_cloud_service.sc.id}/buildServices/default/builders/default"
+  binding_type            = "ApplicationInsights"
+  launch {
+    properties = {
+      sampling_percentage           = "10"
+    }
 
-  build_pack_group {
-    name           = "mix"
-    build_pack_ids = ["tanzu-buildpacks/java-azure"]
-  }
-
-  stack {
-    id      = "io.buildpacks.stacks.bionic"
-    version = "base"
+    secrets = {
+      connection-string = azurerm_application_insights.sc_app_insights.connection_string
+    }
   }
 }
+
 
 # Configuration service
 resource "azurerm_spring_cloud_configuration_service" "configservice" {
