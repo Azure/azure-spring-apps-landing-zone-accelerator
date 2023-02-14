89d3e9d9090e5dd61f5b13a0217f2faad97ece95
ryhud <rhudson@microsoft.com>
2023-02-13 21:05:42 -0500

    switch lookup value for ASA RP from name to AppID

--- Scenarios/ASA-Secure-Baseline/Terraform/03-LZ-Network/spoke_vnet_rbac.tf
+++ Scenarios/ASA-Secure-Baseline/Terraform/03-LZ-Network/spoke_vnet_rbac.tf
@@ -1,7 +1,9 @@
 # RBAC Access for Spoke VNET
+# Azure Spring Apps/Cloud Resource Provider Application ID
+# https://learn.microsoft.com/en-us/azure/spring-apps/how-to-deploy-in-azure-virtual-network?tabs=azure-portal#grant-service-permission-to-the-virtual-network
 
 data "azuread_service_principal" "resource_provider" {
-   display_name = "Azure Spring Cloud Resource Provider"
+   application_id = "e8de9221-a19c-4c81-b814-fd37c6caf9d2"
  }
 
 resource "azurerm_role_assignment" "scowner" {
