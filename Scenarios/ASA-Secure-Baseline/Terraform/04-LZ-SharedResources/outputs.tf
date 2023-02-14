3e48253805e547ba3dcb4eba1a0fe5a2229ec48d
ryhud <rhudson@microsoft.com>
2023-02-13 20:16:50 -0500

    Moving VM outputs to jumphost.tf

--- Scenarios/ASA-Secure-Baseline/Terraform/04-LZ-SharedResources/outputs.tf
+++ Scenarios/ASA-Secure-Baseline/Terraform/04-LZ-SharedResources/outputs.tf
@@ -5,17 +5,3 @@ output shared_rg {
 output law_name {
     value = azurerm_log_analytics_workspace.sc_law.name
 }
-
-output jump_host_name {
-    value = local.jumphost_name
-}
-output jump_host_IP {
-    value = var.jump_host_private_ip_addr
-}
-output jump_host_user {
-    value = local.jumphost_user
-}
-
-output jump_host_pass {
-    value = "To get Password, run: az keyvault secret show --name jumphost-password --vault-name ${azurerm_key_vault.sc_vault.name} -o table"
-}
