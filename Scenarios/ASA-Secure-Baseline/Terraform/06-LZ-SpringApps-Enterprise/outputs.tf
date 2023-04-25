74ec772a6031b071b35870d249e2da052912108a
Jeff Paine <jeff.paine@gmail.com>
2023-04-25 17:03:19 -0400

    Unassign endpoint from gateway and api portal

--- Scenarios/ASA-Secure-Baseline/Terraform/06-LZ-SpringApps-Enterprise/outputs.tf
+++ Scenarios/ASA-Secure-Baseline/Terraform/06-LZ-SpringApps-Enterprise/outputs.tf
@@ -2,11 +2,6 @@ output "spring_apps_service_name" {
   value = azurerm_spring_cloud_service.sc_enterprise.name
 }
 
-output "spring_cloud_gateway_id" {
-  value = azurerm_spring_cloud_gateway.scgateway.id
-}
-
-output "sc_enterprise_registry_id" {
-  value = azurerm_spring_cloud_service.sc_enterprise.service_registry_id
-
+output "spring_apps_rg" {
+  value = azurerm_resource_group.springapps_rg.name
 }
