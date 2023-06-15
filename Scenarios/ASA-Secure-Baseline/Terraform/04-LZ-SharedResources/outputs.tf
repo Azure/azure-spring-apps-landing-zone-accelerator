9c24f806e3ee2eaf62ebf62d7e9c15ed1dc087ef
Jeff Paine <jeff.paine@gmail.com>
2023-06-15 16:19:19 -0500

    Move jumphost outputs to outputs file

--- Scenarios/ASA-Secure-Baseline/Terraform/04-LZ-SharedResources/outputs.tf
+++ Scenarios/ASA-Secure-Baseline/Terraform/04-LZ-SharedResources/outputs.tf
@@ -9,3 +9,13 @@ output "law_name" {
 output "PASSWORD_NOTICE" {
   value = local.password_notice
 }
+
+output "jump_host_name" {
+  value = local.jumphost_name
+}
+output "jump_host_IP" {
+  value = var.jump_host_private_ip_addr
+}
+output "jump_host_user" {
+  value = local.jumphost_user
+}
